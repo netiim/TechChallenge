9a2a9400aa1625730a098e3df0affbbcb98b980a
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-07-27 12:57:50 -0300

    Tentando dar mais tempo para o banco iniciar no github Actions

--- Teste/BaseIntegrationTest.cs
+++ Teste/BaseIntegrationTest.cs
@@ -19,7 +19,33 @@ namespace Testes
             _scope = factory.Services.CreateScope();
             app = factory;
             _context = _scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            WaitForDatabase(_context).Wait();
             _context.Database.MigrateAsync().Wait();
+        }
+        private async Task WaitForDatabase(ApplicationDbContext context)
+        {
+            var retries = 10;
+            var delay = TimeSpan.FromSeconds(5);
+            var connected = false;
+
+            for (int i = 0; i < retries; i++)
+            {
+                try
+                {
+                    await context.Database.OpenConnectionAsync();
+                    connected = true;
+                    break;
+                }
+                catch (Exception)
+                {
+                    await Task.Delay(delay);
+                }
+            }
+
+            if (!connected)
+            {
+                throw new Exception("Unable to connect to the database after multiple attempts.");
+            }
         }
     }
 }
