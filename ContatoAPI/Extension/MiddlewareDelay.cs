c856c5ac2a05de7732fdc108f63c0ce697e3e0f9
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-07-28 17:48:21 -0300

    Ajustes Finais

---
+++ ContatoAPI/Extension/MiddlewareDelay.cs
@@ -0,0 +1,24 @@
+﻿namespace ContatoAPI.Extension
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Threading.Tasks;
+
+    public class RequestDelayMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly int _delayMilliseconds;
+
+        public RequestDelayMiddleware(RequestDelegate next, int delayMilliseconds)
+        {
+            _next = next;
+            _delayMilliseconds = delayMilliseconds;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Adiciona um delay artificial
+            await Task.Delay(_delayMilliseconds);
+            await _next(context);
+        }
+    }
+}
