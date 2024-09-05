7560d4f4ca4a21dfc827221317952044bc7b1bed
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-09-05 19:33:04 -0300

    Api de autenticação com identity configurado

--- AutenticacaoService/AuteticacaoService.API/Program.cs
+++ AutenticacaoService/AuteticacaoService.API/Program.cs
@@ -1,9 +1,19 @@
+using AuteticacaoService.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
 
 var summaries = new[]
 {
