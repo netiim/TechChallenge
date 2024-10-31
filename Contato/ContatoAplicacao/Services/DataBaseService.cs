afecc56ad443830f19d31ed85c49e00ed4f02b78
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-10-31 07:17:18 -0300

    Criando endpoint para migração e ajustando appsettings

---
+++ Contato/ContatoAplicacao/Services/DataBaseService.cs
@@ -0,0 +1,20 @@
+﻿using Contatos.Core.Interfaces.Services;
+using Infraestrutura.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contatos.Aplicacao.Services;
+
+public class DatabaseService : IDatabaseService
+{
+    private readonly ApplicationDbContext _context;
+
+    public DatabaseService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void MigrateDatabase()
+    {
+        _context.Database.Migrate();
+    }
+}
---
+++ Contato/ContatoService/ContatoAPI/Controllers/DatabaseController.cs
@@ -0,0 +1,30 @@
+﻿using Contatos.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContatosService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DatabaseController : ControllerBase
+{
+    private readonly IDatabaseService _databaseService;
+
+    public DatabaseController(IDatabaseService databaseService)
+    {
+        _databaseService = databaseService;
+    }
+
+    [HttpPost("ExecutarMigracaoBanco")]
+    public IActionResult Migrate()
+    {
+        try
+        {
+            _databaseService.MigrateDatabase();
+            return Ok("Database migration applied successfully.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error applying migration: {ex.Message}");
+        }
+    }
+}
