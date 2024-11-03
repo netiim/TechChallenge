5a8e284a845844a570842e8b8e45223b3081e006
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-11-03 19:47:26 -0300

    Ajustes para expor metricas do promethesu

--- Contato/ContatoWorkers/ContatoWorker.Get/Program.cs
+++ Contato/ContatoWorkers/ContatoWorker.Get/Program.cs
@@ -10,10 +10,24 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddInjecoesDependencias();
 builder.Services.AddAutoMapper();
 builder.Services.AddFluentValidation();
-
 builder.Services.AddDatabaseConfiguration(builder.Configuration);
-
 builder.Services.AddMassTransitWithRabbitMq(builder.Configuration);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
+
+// Configuração do middleware para expor métricas
+builder.WebHost.Configure(app =>
+{
+    app.UseRouting();
+    app.UseHttpMetrics(); // Middleware para capturar métricas HTTP
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapMetrics(); // Endpoint /metrics para Prometheus
+    });
+});
 
 var host = builder.Build();
 host.Run();
